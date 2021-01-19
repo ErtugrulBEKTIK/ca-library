f33d8eb7c470af58ea205a5a996780a4573a6e48
Ertuğrul BEKTİK <ertugrulbektik99@gmail.com>
2021-01-19 12:50:48 +0300

    Cors problem solving...

--- middlewares.py
+++ middlewares.py
@@ -29,6 +29,12 @@ def initialize_middlewares(app):
         return Response('You are not allowed to enter', 401)
 
       
+  @app.after_request
+  def after_request(response):
+      response.headers.add('Access-Control-Allow-Origin', '*')
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+      response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+      return response
   
 
       
